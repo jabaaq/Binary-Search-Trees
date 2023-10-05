f2fd1442084ac67228f3b0de6f66df9d5b0b4f77
Jaba <jabaqadagishvili4@gmail.com>
2023-10-05 14:42:41 +0400

    Created insert,  delete and find (Which accepts a value and returns the node with the given value.)

--- binaryTrees.js
+++ binaryTrees.js
@@ -37,8 +37,62 @@ class Tree {
 
         return root
     }
+
+    insert(value, root = this.root) {
+        if (root === null) {
+            return root = new Node(value)
+        }
+
+        if (root.data < value) {
+            root.right = this.insert(value, root.right)   //insert to the right
+        } else {
+            root.left = this.root(value, root.left)         //insert to the left
+        }
+
+        prettyPrint(this.root)
+        return root
+    }
+
+    delete(value, root = this.root) {
+        if (root === null) {
+            return root
+        }
+
+        if (root.data > value) {
+            root.left = this.delete(value, root.left)
+        } else if (root.data < value) {
+            root.right = this.delete(value, root.right)
+        } else {
+            if (root.left === null) {
+                return root.right
+            } else if (root.right === null) {
+                return root.left
+            }
+            root.right = this.delete(root.right, root.data)
+        }
+        prettyPrint(this.root)
+        return root
+    }
+
+    find(value, root = this.root) {
+        if (root === null) return false
+
+        if (root.data == value) return root
+
+        if (root.data > value) {
+            return this.find(value, root.left)
+        } else if (root.data < value) {
+            return this.find(value, root.right)
+        }
+        prettyPrint(this.root)
+        return root
+    }
 }
 
 let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const binaryTree = new Tree(testArray, 1, 9)
+binaryTree.delete(3)
+binaryTree.insert(11)
+console.log(binaryTree.find(4));       // Node { data: 4, left: null, right: null }
+console.log(binaryTree.find(10));       //false
